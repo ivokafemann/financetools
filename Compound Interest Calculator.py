7fbf5f78025d1d7c9b32c9057178a7f32ae63bb9
Ivo-Kafemann <ivokafemann@gmail.com>
2020-04-06 10:29:59 +0200

    Define parameters for compound interest calculator

--- Compound Interest Calculator.py
+++ Compound Interest Calculator.py
@@ -1,6 +1,5 @@
-def interest_calculator():
-    """
-    Asks the user how much money, at which interest rate he wants to invest
-    """
 
-    
+
+A = input('\nHow much money would you like to invest?\n')
+r = input('\nWhat is the interest rate (use e.g. .05 for 5 % interest rate) for the investment?\n')
+t = input('\nWhat is the ?\n')
