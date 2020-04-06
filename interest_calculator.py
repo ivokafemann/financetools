7bbc91590f870f6caa9ceff6e136df321f1d20a6
Ivo-Kafemann <ivokafemann@gmail.com>
2020-04-06 10:45:20 +0200

    Write the formula for interest_calculator.py

---
+++ interest_calculator.py
@@ -0,0 +1,20 @@
+"""
+A small tool to calculate the value of an investment in the future.
+
+Asks the user how much money, at which interest rate he wants to invest.
+
+Args:
+    (int) I - value of initial investment
+    (int) r - interest rate
+    (int) t - time in years
+Returns:
+    (int) A - Value of investment in the future
+"""
+
+I = int(input('\nHow much money would you like to invest?\n'))
+r = int(input('\nWhat is the interest rate (write only 5 for an interest rate 5 %) for the investment?\n'))
+t = int(input('\nWhat is the time horizon in years for the investment?\n'))
+
+A = I * (1 + r/100)**t
+
+print('\nThe value of your investment after {} year(s) at an interest rate of {}% is: {}'.format(t,r,A))
