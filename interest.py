add6acd5fd2249efb5cfc31adccd731f8cac9a8a
Ivo-Kafemann <ivokafemann@gmail.com>
2020-04-10 11:55:17 +0200

    Rename the functions

--- interest.py
+++ interest.py
@@ -16,25 +16,25 @@ r = int(input('\nWhat is the interest rate (write only 5 for an interest rate 5 
 t = int(input('\nWhat is the time horizon in years for the investment?\n'))
 
 #Annuity Function
-def annuity(P,r,t):
+def compound_interest(P,r,t):
     P1 = P * (1 + r/100)**t
     return P1
 
-print('\nThe value of your investment after {} year(s) at an interest rate of {}% is: {}'.format(t,r,annuity(P,r,t)))
+print('\nThe value of your investment after {} year(s) at an interest rate of {}% is: {}'.format(t,r,compound_interest(P,r,t)))
 
 #Create a list of annuities with the Annuities Function
-def annuities(P,r,t):
-    P1 = [annuity(P,r,x) for x in range(t)]
+def compound_interests(P,r,t):
+    P1 = [compound_interest(P,r,x) for x in range(t)]
     return P1
 
-print('\nThe annuity for each year is shown in the following list:\n',annuities(P,r,t))
+print('\nThe annuity for each year is shown in the following list:\n',compound_interests(P,r,t))
 print('\nThe following graph plots the annuities over the given time horizon T\n')
 
 tL = list(range(t))
 
 #Plot the annuities over the time horizon T
 import matplotlib.pyplot as plt
-plt.plot(tL,annuities(P,r,t))
+plt.plot(tL,compound_interests(P,r,t))
 plt.xlabel("t [y]")
 plt.ylabel("Value [€]")
 plt.title("Compound Interest")
