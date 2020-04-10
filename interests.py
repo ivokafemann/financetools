03ab81c2659bbfad650a19b7fe46e4c7c4506dbe
Ivo-Kafemann <ivokafemann@gmail.com>
2020-04-10 15:03:24 +0200

    Define compound_interest function

--- interests.py
+++ interests.py
@@ -17,5 +17,9 @@ r = str(input('\nEnter any number of interest rates you would like to compare (s
 t = int(input('\nWhat is the time horizon in years for the investment?\n'))
 
 rL = [int(i) for i in r.split()]
-print(rL)
 tL = list(range(t+1))
+
+#Compound Interest Function
+def compound_interest(P,r,t):
+    Pt = P * (1 + r/100)**t
+    return Pt
