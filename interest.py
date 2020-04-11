8c6b5b713b856c36675dab857a1b6dc6a4b36e85
Ivo-Kafemann <ivokafemann@gmail.com>
2020-04-11 13:28:42 +0200

    Rename function: compound_interests -> compound_interest_L

--- interest.py
+++ interest.py
@@ -23,18 +23,18 @@ def compound_interest(P,r,t):
 print('\nThe value of your investment after {} year(s) at an interest rate of {}% is: {}'.format(t,r,compound_interest(P,r,t)))
 
 #Create a list of compound interests for all moments of the time horizon with the compound_interest function
-def compound_interests(P,r,t):
+def compound_interest_L(P,r,t):
     PL = [compound_interest(P,r,x) for x in range(t)]
     return PL
 
-print('\nThe new principal sum at each year is shown in the following list:\n',compound_interests(P,r,t))
+print('\nThe new principal sum at each year is shown in the following list:\n',compound_interest_L(P,r,t))
 print('\nThe following graph plots the growth of the principal sum over the given time horizon t = {} year(s)'.format(t))
 
 tL = list(range(t+1))
 
 ##Plot the growth of the principal sum over the time horizon t at the given rate r
 import matplotlib.pyplot as plt
-plt.plot(tL,compound_interests(P,r,t))
+plt.plot(tL,compound_interest_L(P,r,t))
 plt.xlabel("t [y]")
 plt.ylabel("Value [€]")
 plt.title("Compound Interest")
